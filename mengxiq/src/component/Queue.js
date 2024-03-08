c7e3a11e32c1a6c9f6b09cde2c4fdcf05b0d2b33
Mengxi Zhang <zmx94824@gmail.com>
2024-03-08 15:57:03 -0800

    use bootstrap 5

--- mengxiq/src/component/Queue.js
+++ mengxiq/src/component/Queue.js
@@ -2,16 +2,14 @@ import QueueItem from "./QueueItem";
 
 function Queue() {
 	const items = [
-		{description: "Ask Ann for task1", link: "https://abd.talk.com", priority: "Do it now"},
+		{description: "Ask Ann for task1, this is a very very very long description", link: "https://abd.talk.com", priority: "Do it now"},
 		{description: "Spike why B happened", link: "https://abd.talk.com", priority: "Moon shooting"}
 	]
 	return (
 		<div>
 			<table>
 				<tr>
-					<td><button style={{width: '100%'}}>Add item</button></td>
-					<td>Description: <input/></td>
-					<td>Link: <input/></td>
+					<td>Link: <input style={{width: '500px'}}/></td>
 					<td>
 						<select>
 							<option>Do it now</option>
@@ -22,7 +20,15 @@ function Queue() {
 					</td>
 				</tr>
 			</table>
-			<table>
+			<div class="input-group" style={{width: '90%', height: '200px'}}>
+				<span class="input-group-text">Description</span>
+				<textarea class="form-control" aria-label="With textarea"></textarea>
+			</div>
+			<div style={{width: '90%'}}>
+				<button style={{width: '65%'}}>Add item</button>
+				<button style={{width: '35%'}}>Hide</button>
+			</div>
+			<table style={{'margin-top': '15px'}}>
 				{items.map(function(d){
          	return <QueueItem
 						description={d.description}
