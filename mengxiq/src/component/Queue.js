af833b68b9a489333eb9cb3c2391e85c463271c6
Mengxi Zhang <zmx94824@gmail.com>
2024-03-09 15:58:12 -0800

    fix indentation

--- mengxiq/src/component/Queue.js
+++ mengxiq/src/component/Queue.js
@@ -5,20 +5,20 @@ import { v4 as uuidv4 } from 'uuid';
 import { priorityLevelMap, priorityLevelMapKeys } from "./Common"
 
 function Queue() {
-	const [items, setItems] = useState([]);
-	const [curDescription, setCurDescription] = useState("");
-	const [curLink, setCurLink] = useState("");
-	const [curPriorityId, setCurPriorityId] = useState("");
+  const [items, setItems] = useState([]);
+  const [curDescription, setCurDescription] = useState("");
+  const [curLink, setCurLink] = useState("");
+  const [curPriorityId, setCurPriorityId] = useState("");
 
-	function handleAddItem() {
-		const newItems = items.slice();
-		newItems.push({
-			description: curDescription,
-			link: curLink,
-			id: uuidv4(),
+  function handleAddItem() {
+    const newItems = items.slice();
+    newItems.push({
+      description: curDescription,
+      link: curLink,
+      id: uuidv4(),
       created_time: new Date(),
       priorityId: curPriorityId
-		});
+    });
     newItems.sort((a, b) => {
       let cmp = priorityLevelMap[b.priorityId].rank - priorityLevelMap[a.priorityId].rank;
       if (cmp !== 0) {
@@ -27,67 +27,67 @@ function Queue() {
         return a.created_time - b.created_time;
       }
     })
-		setItems(newItems);
-	}
+    setItems(newItems);
+  }
 
-	function deleteItem(id) {
-		// console.log("deleteItem function called with id: " + id);
-		const newItems = items.filter(e => e.id !== id).slice();
-		// console.log("new items: ");
-		// console.log(newItems);
-		setItems(newItems);
-	}
+  function deleteItem(id) {
+    // console.log("deleteItem function called with id: " + id);
+    const newItems = items.filter(e => e.id !== id).slice();
+    // console.log("new items: ");
+    // console.log(newItems);
+    setItems(newItems);
+  }
 
-	return (
-		<div>
-			<table>
-				<tr>
-					<td>
-						Link: <input onChange={(e) => setCurLink(e.target.value)} style={{width: '500px'}}/>
-					</td>
-					<td>
-						<select onChangeCapture={(e) => setCurPriorityId(e.target.value)}>
-							{
-                priorityLevelMapKeys.map(function(id){
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>
+            Link: <input onChange={(e) => setCurLink(e.target.value)} style={{ width: '500px' }} />
+          </td>
+          <td>
+            <select onChangeCapture={(e) => setCurPriorityId(e.target.value)}>
+              {
+                priorityLevelMapKeys.map(function (id) {
                   return <option value={id}>{priorityLevelMap[id].display}</option>
                 })
               }
-						</select>
-					</td>
-				</tr>
-			</table>
-			<div style={{width: '90%'}}>
-				<span style={{width: '10%'}}>Description</span>
-				<TextInput 
-					multiline={true}
-					numberOfLines={5}
-					style={{width: '90%'}}
-					onChangeText={text => setCurDescription(text)}
-					placeholder="Enter item description"
-				/>
-			</div>
-			<div style={{width: '90%'}}>
-				<button
-					style={{width: '65%'}}
-					onClick={handleAddItem}
-				>
-					Add item
-				</button>
-				<button style={{width: '35%'}}>Hide</button>
-			</div>
+            </select>
+          </td>
+        </tr>
+      </table>
+      <div style={{ width: '90%' }}>
+        <span style={{ width: '10%' }}>Description</span>
+        <TextInput
+          multiline={true}
+          numberOfLines={5}
+          style={{ width: '90%' }}
+          onChangeText={text => setCurDescription(text)}
+          placeholder="Enter item description"
+        />
+      </div>
+      <div style={{ width: '90%' }}>
+        <button
+          style={{ width: '65%' }}
+          onClick={handleAddItem}
+        >
+          Add item
+        </button>
+        <button style={{ width: '35%' }}>Hide</button>
+      </div>
 
-			<table style={{'margin-top': '15px'}}>
-				{items.map(function(d){
-         	return <QueueItem
-						description={d.description}
-						link={d.link}
-						priorityId={d.priorityId}
-						deleteFuncion={() => deleteItem(d.id)}
-					/>
-       	})}
-			</table>
-		</div>
-	);
+      <table style={{ 'margin-top': '15px' }}>
+        {items.map(function (d) {
+          return <QueueItem
+            description={d.description}
+            link={d.link}
+            priorityId={d.priorityId}
+            deleteFuncion={() => deleteItem(d.id)}
+          />
+        })}
+      </table>
+    </div>
+  );
 }
 
 export default Queue;
--- mengxiq/src/component/QueueItem.js
+++ mengxiq/src/component/QueueItem.js
@@ -1,17 +1,17 @@
 import { priorityLevelMap } from "./Common"
 
 function QueueItem({ description, link, priorityId, deleteFuncion }) {
-	return (
-		<tr>
-			<td>{description}</td>
-			<td><a href={link}>{link}</a></td>
-			<td>{priorityLevelMap[priorityId].display}</td>
-			<td>
-				<button>Edit</button>
-				<button onClick={deleteFuncion}>Done/Delete</button>
-			</td>
-		</tr>
-	);
+  return (
+    <tr>
+      <td>{description}</td>
+      <td><a href={link}>{link}</a></td>
+      <td>{priorityLevelMap[priorityId].display}</td>
+      <td>
+        <button>Edit</button>
+        <button onClick={deleteFuncion}>Done/Delete</button>
+      </td>
+    </tr>
+  );
 }
 
 export default QueueItem;
